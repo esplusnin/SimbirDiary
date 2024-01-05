48a7701f0214466be7940063eac4eedc62a566a6
esplusnin <esplusnin94@yandex.ru>
2024-01-05 13:43:30 +0400

    №1 Задать стартовый сетап проекта
    1)таргет 12
    2)сетап проекта под вёрстку кодом

--- SimbirDiary/ViewController.swift
+++ SimbirDiary/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  SimbirDiary
-//
-//  Created by Евгений on 05.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
 
